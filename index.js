4efb9be7bbaef742a82fbeee4cd755a637e6a644
orangemn6 <jbensa081@gmail.com>
2021-02-24 17:57:42 -0500

    everything

--- index.js
+++ index.js
@@ -2,5 +2,41 @@ var Discord = require('discord.js');
 require('dotenv').config()
 var client = new Discord.Client();
 var token = process.env.TOKEN;
-var commandprefix = "!"
+var prefix = "!"
+
+/*client.on('message', message => {
+
+if (!message.content.startsWith(prefix) || message.author.bot) {
+	message.channel.send('Worked!')
+};
+const args = message.content.slice(prefix.length).trim().split(' ');
+const command = args.shift().toLowerCase();
+});*/
+
+client.on('message', message => {
+
+if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+const args = message.content.slice(prefix.length).trim().split(' ');
+const command = args.shift().toLowerCase();
+
+    if (message.content === `${prefix}ping`) {
+        message.channel.send('Pong.');
+    } else if (command === 'request') {
+    	if (!args.length) {
+    		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    	}
+
+		else if (args[0] === 'foo') {
+				return message.channel.send('bar');
+			}
+
+
+
+    
+    	message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    }
+});
+
+
 client.login(token);
